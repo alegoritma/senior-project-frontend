294d62ecfc972417508809c84c7f69574e11c70c
Volkan <volkankalin07@gmail.com>
2022-06-04 13:12:02 +0300

    contact page implemented

---
+++ src/pages/Contact/ContactMap.tsx
@@ -0,0 +1,11 @@
+export const ContactMap = () => (
+  <iframe
+    src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7589.1403280370905!2d30.65044453369033!3d36.89416501305847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c391d21adab4e3%3A0x3565209148a2e448!2sAkdeniz%20University%20Faculty%20of%20Engineering!5e0!3m2!1sen!2str!4v1654333770004!5m2!1sen!2str'
+    width='100%'
+    height='450'
+    className='MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation5'
+    style={{ border: 0 }}
+    // allowFullScreen=''
+    loading='lazy'
+    referrerPolicy='no-referrer-when-downgrade'></iframe>
+);
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,78 @@
-import { Box } from '@mui/material';
+import React from 'react';
+import { Box, Card, CardContent, Divider, Paper, Stack, Typography } from '@mui/material';
+import { ContactMap } from 'src/pages/Contact/ContactMap';
+
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EmailIcon from '@mui/icons-material/Email';
+
+const CONTACT_CARDS = [
+  {
+    Icon: MapsHomeWorkIcon,
+    title: 'Place',
+    text: 'Akdeniz University'
+  },
+  {
+    Icon: PhoneIphoneIcon,
+    title: 'Phone',
+    text: '+90 242 227 4400 (internal 4361)'
+  },
+  {
+    Icon: LocationOnIcon,
+    title: 'Address',
+    text: 'Akdeniz University\nFaculty of Engineering\nComputer Engineering Department\nAntalya Campus\nDumlupınar Boulevard\n07058 Antalya / TURKEY'
+  },
+  {
+    Icon: EmailIcon,
+    title: 'Email',
+    text: 'cse at akdeniz.edu.tr'
+  }
+];
+
+interface ContactCardProps {
+  Icon: React.ElementType<any>;
+  title: string;
+  text: string;
+}
+
+const ContactCard: React.FC<ContactCardProps> = ({ Icon, title, text }) => {
+  return (
+    <Box>
+      <Stack direction='row'>
+        <Icon fontSize='large' htmlColor='#3193a1' />
+        <Box ml={0.5}>
+          <Typography variant='h2' mt={0.5} mb={1}>
+            {title}
+          </Typography>
+          <Typography
+            color='#4e555b'
+            sx={{ whiteSpace: 'pre', fontFamily: "'Poppins', sans-serif" }}>
+            {text}
+          </Typography>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
 
 function Contact() {
-  return <Box>Contact</Box>;
+  return (
+    <Box>
+      <Typography variant='h1' mt={4} mb={2} textAlign='center' fontSize={'3rem'}>
+        Contact Us
+      </Typography>
+      <Divider />
+      <Stack direction='row' justifyContent='space-around' mt={5}>
+        {CONTACT_CARDS.map((props, i) => (
+          <ContactCard {...props} key={`contact-card-${i}`} />
+        ))}
+      </Stack>
+      <Box sx={{ mt: 6 }}>
+        <ContactMap />
+      </Box>
+    </Box>
+  );
 }
 
 export default Contact;
