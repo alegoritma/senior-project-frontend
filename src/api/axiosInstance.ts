0e36c1f2ba632116d35c1f834db7d09a54b25dad
Volkan <volkankalin07@gmail.com>
2022-06-04 15:47:58 +0300

    add cloud base url

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001/'
+  // baseURL: 'http://localhost:3001/'
+  baseURL: 'https://vetheal-app.herokuapp.com/'
+  // baseURL: 'http://localhost:8000/'
 });
