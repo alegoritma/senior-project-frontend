ed210232407e60b74ffe9e048549daffc27f51ca
Volkan <volkankalin07@gmail.com>
2022-06-04 15:40:24 +0300

    add back button to symptom page

--- src/pages/SymptomForm/Container.tsx
+++ src/pages/SymptomForm/Container.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Card, Box, CardContent, CardMedia, Divider, Typography } from '@mui/material';
+import { Card, Box, CardContent, CardMedia, Divider, Typography, IconButton } from '@mui/material';
 import { useSelector } from 'src/store';
-
+import { useNavigate } from 'react-router';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 interface Props {
   animalId: string;
 }
 
 const Banner: React.FC<Props> = ({ animalId }) => {
-  const { animals, loading } = useSelector((state) => state.animals);
-
+  const { animals } = useSelector((state) => state.animals);
+  const navigate = useNavigate();
   // if (loading) return <p>Loading...</p>;
 
   const animal = animals[animalId];
@@ -16,11 +17,30 @@ const Banner: React.FC<Props> = ({ animalId }) => {
   return (
     <Box>
       <CardMedia component='img' height='440' image={animal?.image} />
-      <CardContent>
-        <Typography variant='h1' component='div'>
-          {animal?.name ?? '---'}
-        </Typography>
-      </CardContent>
+      <Box height={0} mb={1}>
+        <Box sx={{ transform: 'translateY(-100%)' }}>
+          <Box
+            sx={{ background: 'linear-gradient(0, black, transparent)' }}
+            p={2}
+            pl={0}
+            pt={3}
+            pb={1}
+            display='flex'>
+            <IconButton
+              onClick={() => navigate(-1)}
+              size='small'
+              sx={{ color: (theme) => theme.palette.grey[200] }}>
+              <ChevronLeftIcon fontSize='large' />
+            </IconButton>
+            <Typography
+              sx={{ fontSize: '2rem', color: (theme) => theme.palette.grey[200] }}
+              variant='h1'
+              component='div'>
+              {animal?.name ?? '---'}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
